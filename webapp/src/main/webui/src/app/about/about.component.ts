5178d122534e44b8949871d1d36a4822a82e30ab
rrowlands <l2.rowlands@gmail.com>
2024-08-26 11:28:49 -0600

    Flashy animations

--- webapp/src/main/webui/src/app/about/about.component.ts
+++ webapp/src/main/webui/src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -8,9 +8,17 @@ import { RouterModule } from '@angular/router';
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+
+  public isPreload = true;
 
   constructor() { }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isPreload = false;
+    }, 100);
+  }
 
   public captureEmailForm(): void {
     window.location.href = "https://2d35a37e.sibforms.com/serve/MUIFABzv_pK1_YgaT0O9h369Fe89iBz1lmE63oAo2cuHjvcQmATp3Juz4BudHm6zdwwIAraE4YGla-0G121m2DEC-RQP_YUO98T5a5ciR33HDYJnFAyYATNoiO6H5PQWTPfkfYJOae2Rx_J52Ag3H4B8I--ljBdvugyb0oQdfxaOFEamGNOGHPfBEaEA-yFacsvAN7oZRyaOXKcB";
